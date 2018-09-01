0790e221a2622da772947d2f3bbec6420a421db7
k4meli4 <kgracheva@gmail.com>
2018-08-31 20:56:30 -0400

    first draft

---
+++ README.md
@@ -0,0 +1,32 @@
+liri-node-app
+
+LIRI is a Language Interpretation and Recognition Interface. Use LIRI to get your latest tweets, find out about a song, or a movie, or just choose a random action from your own random file.
+
+
+Get Started
+
+Commands you can use in LIRI.
+
+Get Tweets
+
+Retrieves up to your latest 20 tweets:
+
+node liri.js my-tweets
+
+Get Song Info
+
+Retrieves song information for a track:
+
+node liri.js spotify-this-song "American Girl"
+
+Get Movie Info
+
+Retrieves movie information for a movie:
+
+node liri.js movie-this "Star Wars"
+
+Get Random Info
+
+Gets random text inside a file and does what it says:
+
+node liri.js do-what-it-says
---
+++ liri.js
@@ -0,0 +1,116 @@
+require("dotenv").config();
+
+var keys = require('./keys.js');
+
+var Twitter = require('twitter');
+
+var spotify = require('spotify');
+
+var request = require('request');
+
+var fs = require('fs');
+
+// var spotify = new Spotify(keys.spotify);
+
+
+var spotify = require('spotify');
+
+var getMyTweets = function(){
+
+    var client = new Twitter(keys.twitterKeys);
+
+    var params = {screen_name: 'thescreenname'};
+    client.get('statuses/user_timeline', params, function(error, tweets,
+        response) {
+            if (!error) {
+                for(var i=0; i<tweets.length; i++) {
+                    console.log(tweets[i].created_at);//when the tweet was created
+                    console.log(' '); //space
+                    console.log(tweets[i].text);//text of the tweet
+            }
+        }
+    });
+} 
+
+var getArtistNames = function(artist) {
+    return artist.name;
+}
+
+var getMeSpotify = function(songName) {
+    
+spotify.search({type: 'track', query: songName }
+    , function(err, data){
+    if (err) {
+        console.log('Error occured: ' + err);
+        return;
+    }
+    var songs = data.tracks.items;
+    for(var i=0; i<songs.length; i++) {
+        console.log(i);
+        console.log('artist(s): ' + songs[i].artists.map(
+            getArtistNames));
+        console.log('song name: ' + songs[i].name);
+        console.log('preview song: ' + songs[i].preview_url);
+        console.log('album: ' + songs[i].album.name);
+        console.log('-------------------------------------');
+    }
+});
+
+}
+
+var getMeMovie = function(movieName) {
+
+    request('http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&apikey=trilogy', function(error, response, body){
+        if(!error && response.statusCode == 200){
+            
+            var jsonData = JSON.parse(body);
+
+            console.log('Title: ' + jsonData.Title);
+            console.log('Year: ' + jsonData.Year);
+            console.log('Rated: ' + jsonData.Rated);
+            console.log('IMDB Rating: ' + jsonData.imdbRating);
+            console.log('Country: ' + jsonData.Country);
+            console.log('Language: ' + jsonData.Language);
+            console.log('Plot: ' + jsonData.Plot);
+            console.log('Actors: ' + jsonData.Actors);
+            console.log('Rotten tomatoes rating: ' + jsonData.tomatoRating);
+            console.log('Rotten tomatoes URL: ' + jsonData.tomatoURL);
+        }
+    });
+}
+var doWhatItSays = function(){
+fs.readFile('random.txt', 'utf8', function(err, data) {
+    if (err) throw err;
+   
+    var dataArr = data.split(',');
+
+    if(dataArr.length == 2) {
+        pick(dataArr[0], dataArr[1]);
+    } else if (dataArr.length == 1){
+        pick(dataArr[0]);
+    }
+  });
+}
+var pick = function(caseData, functionData) {
+    switch(caseData) {
+        case 'my-tweets'  :
+            getMyTweets();
+            break;
+        case 'spotify-this-song':
+            getMeSpotify(functionData);
+            break; 
+        case 'movie-this':
+            getMeMovie(functionData);
+        case 'do-what-it-says':
+            doWhatItSays();
+            break;
+        default:
+        console.log('LIRI does not know that');
+    }
+}
+
+var runThis = function(argOne, argTwo) {
+    pick(argOne, argTwo);
+};
+
+runThis(process.argv[2], process.argv[3]); 
