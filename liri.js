0634178a524d3e5f2b34b9cacd6b245bcb9d66bd
k4meli4 <kgracheva@gmail.com>
2018-09-13 21:01:35 -0400

    everything working but spotify starter file has a bug

--- liri.js
+++ liri.js
@@ -31,33 +31,33 @@ var getMyTweets = function(){
     });
 } 
 
-// var getArtistNames = function(artist) {
-//     return artist.name;
-// }
+var getArtistNames = function(artist) {
+    return artist.name;
+}
 
-// var getMeSpotify = function(songName) {
-//     console.log(Spotify);
+var getMeSpotify = function(songName) {
+    console.log(Spotify);
     
-// spotify.search({type: 'track', query: songName }
-//     , function(err, data){
-//     if (err) {
-//         console.log('Error occured: ' + err);
-//         return;
-//     }
-//     console.log(data);
-//     var songs = data.tracks.items;
-//     for(var i=0; i<songs.length; i++) {
-//         console.log(i);
-//         console.log('artist(s): ' + songs[i].artists.map(
-//             getArtistNames));
-//         console.log('song name: ' + songs[i].name);
-//         console.log('preview song: ' + songs[i].preview_url);
-//         console.log('album: ' + songs[i].album.name);
-//         console.log('-------------------------------------');
-//     }
-// });
+spotify.search({type: 'track', query: songName }
+    , function(err, data){
+    if (err) {
+        console.log('Error occured: ' + err);
+        return;
+    }
+    console.log(data);
+    var songs = data.tracks.items;
+    for(var i=0; i<songs.length; i++) {
+        console.log(i);
+        console.log('artist(s): ' + songs[i].artists.map(
+            getArtistNames));
+        console.log('song name: ' + songs[i].name);
+        console.log('preview song: ' + songs[i].preview_url);
+        console.log('album: ' + songs[i].album.name);
+        console.log('-------------------------------------');
+    }
+});
 
-// }
+}
 
 var getMeMovie = function(movieName) {
 
